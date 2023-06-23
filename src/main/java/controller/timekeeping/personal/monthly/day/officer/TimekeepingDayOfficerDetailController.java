e30e1cdffabd0485350dd2bd5d278538d6ff6021
HungND1701 <Hung.nd204984@sis.hust.edu.vn>
2023-06-23 13:46:48 +0700

    Hung: logout + title + dashboard

--- src/main/java/controller/timekeeping/personal/monthly/day/officer/TimekeepingDayOfficerDetailController.java
+++ src/main/java/controller/timekeeping/personal/monthly/day/officer/TimekeepingDayOfficerDetailController.java
@@ -89,9 +89,10 @@ public class TimekeepingDayOfficerDetailController {
     	
     	salaryCol.setCellValueFactory(cellData -> {
             // Perform the salary calculation based on the Employee object
-    		LogTimekeepingOfficer log = cellData.getValue();
+    		if(log.getTime_in() != null) { log = cellData.getValue();
             int salary = calculateSalary(log);
             return new SimpleIntegerProperty(salary).asObject();
+    		}else return new SimpleIntegerProperty(0).asObject();
         });
     	
     	
